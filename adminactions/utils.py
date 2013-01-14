d2391861a91e19efe8bd688b2170d7e172c126ef
sax <s.apostolico@gmail.com>
2013-01-14 09:25:14 +0700

    utils

--- adminactions/utils.py
+++ adminactions/utils.py
@@ -1,3 +1,30 @@
+from django.db import models
+
+
+def get_field_from_path(Model, field_path):
+    """
+    get a Model and a path to a attribute, return the field
+
+
+    >>> a = get_field_from_path(Payslip, 'contract.employee.health_ins_type')
+    >>> print a
+    Health Insurance Type
+    """
+    parts = field_path.split('.')
+    target = parts[0]
+    if target in Model._meta.get_all_field_names():
+        field_object, model, direct, m2m = Model._meta.get_field_by_name(target)
+        if isinstance(field_object, models.fields.related.ForeignKey):
+            if parts[1:]:
+                return get_field_from_path(field_object.rel.to, '.'.join(parts[1:]))
+            else:
+                return field_object
+        else:
+            return field_object
+    return None
+
+
+
 def flatten(x):
     """flatten(sequence) -> list
 
