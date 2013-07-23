958cfe45b89de2be796c9e545ed07349cb1c7b9a
Julien Aubert <julien.aubert@wfp.org>
2013-07-23 23:14:29 +0200

    fixes in export_as_csv and add export_as_xls (api only)
    export_as_csv: add dialect. add alternative header. fix filename kwarg. filename kwarg was required even when using out kwarg
    
    add export_as_xls (only as api, not within user interface)

--- adminactions/api.py
+++ adminactions/api.py
@@ -5,6 +5,7 @@ from django.db import transaction
 from django.db.models.fields.related import ManyToManyField, OneToOneField
 from django.http import HttpResponse
 from adminactions.templatetags.actions import get_field_value
+import xlwt
 try:
     import unicodecsv as csv
 except ImportError:
@@ -110,7 +111,8 @@ def merge(master, other, fields=None, commit=False, m2m=None, related=None):
     return result
 
 
-def export_as_csv(queryset, fields=None, header=False, filename=None, options=None, out=None):
+def export_as_csv(queryset, fields=None, header=False, filename=None, options=None, out=None, header_alt=None,
+                  dialect=None):
     """
         Exports a queryset as csv from a queryset with the given fields.
 
@@ -119,12 +121,13 @@ def export_as_csv(queryset, fields=None, header=False, filename=None, options=No
     :param header: if True, the exported file will have the first row as column names
     :param filename: name of the filename
     :param options: CSVOptions() instance or none
-    :param: out: object that implements File protocol. HttpResponse if None.
-
-    :return: HttpResponse instance
+    :param out: object that implements File protocol. HttpResponse if None.
+    :param header_alt: if is not None, and header is True, the first row will be as header_alt (same nr columns)
+    :param dialect: if is not None, will use this instead of options for csv writer, (see dialects in unicodecsv)
+    :return: HttpResponse instance if out not supplied, otherwise out
     """
-    filename = filename or "%s.csv" % queryset.model._meta.verbose_name_plural.lower().replace(" ", "_")
     if out is None:
+        filename = filename or "%s.csv" % queryset.model._meta.verbose_name_plural.lower().replace(" ", "_")
         response = HttpResponse(mimetype='text/csv')
         response['Content-Disposition'] = 'attachment;filename="%s"' % filename.encode('us-ascii', 'replace')
     else:
@@ -139,13 +142,20 @@ def export_as_csv(queryset, fields=None, header=False, filename=None, options=No
     if fields is None:
         fields = [f.name for f in queryset.model._meta.fields]
 
-    writer = csv.writer(response,
-                        escapechar=str(config['escapechar']),
-                        delimiter=str(config['delimiter']),
-                        quotechar=str(config['quotechar']),
-                        quoting=int(config['quoting']))
+    if dialect is not None:
+        writer = csv.writer(response, dialect=dialect)
+    else:
+        writer = csv.writer(response,
+                            escapechar=str(config['escapechar']),
+                            delimiter=str(config['delimiter']),
+                            quotechar=str(config['quotechar']),
+                            quoting=int(config['quoting']))
+
     if header:
-        writer.writerow([f for f in fields])
+        if header_alt is not None:
+            writer.writerow([f for f in header_alt])
+        else:
+            writer.writerow([f for f in fields])
     for obj in queryset:
         row = []
         for fieldname in fields:
@@ -159,4 +169,66 @@ def export_as_csv(queryset, fields=None, header=False, filename=None, options=No
             row.append(smart_str(value))
         writer.writerow(row)
 
+    return response
+
+
+formatting_default = {'date_format': 'd/m/Y',
+                      'datetime_format': 'N j, Y, P',
+                      'time_format': 'P', }
+
+
+def export_as_xls(queryset, sheet_name=None, fields=None, header=False, header_alt=None, filename=None,
+                  formatting=None, out=None):
+    """
+    Exports a queryset as xls from a queryset with the given fields.
+
+    :param queryset: queryset to export (can also be list of namedtuples)
+    :param fields: list of fields names to export. None for all fields
+    :param header: if True, the exported file will have the first row as column names
+    :param out: object that implements File protocol.
+    :param header_alt: if is not None, and header is True, the first row will be as header_alt (same nr columns)
+    :param formatting: if is None will use formatting_default
+    :return: HttpResponse instance if out not supplied, otherwise out
+    """
+    if out is None:
+        filename = filename or "%s.csv" % queryset.model._meta.verbose_name_plural.lower().replace(" ", "_")
+        response = HttpResponse(mimetype='text/csv')
+        response['Content-Disposition'] = 'attachment;filename="%s"' % filename.encode('us-ascii', 'replace')
+    else:
+        response = out
+    book = xlwt.Workbook(encoding="UTF-8")
+    if sheet_name is None:
+        sheet_name = 'Sheet1'
+    sheet = book.add_sheet(sheet_name)
+    if fields is None:
+        fields = [f.name for f in queryset.model._meta.fields]
+
+    if formatting is None:
+        formatting = formatting_default
+
+    if header_alt:
+        header_row = header_alt
+    else:
+        header_row = fields
+
+    def write_row(row_nr, row):
+        for col_nr, cell_value in enumerate(row):
+            sheet.write(row_nr, col_nr, cell_value)
+
+    if header:
+        write_row(0, header_row)
+
+    for row_nr, obj in enumerate(queryset):
+        row = []
+        for fieldname in fields:
+            value = get_field_value(obj, fieldname)
+            if isinstance(value, datetime.datetime):
+                value = dateformat.format(value, formatting['datetime_format'])
+            elif isinstance(value, datetime.date):
+                value = dateformat.format(value, formatting['date_format'])
+            elif isinstance(value, datetime.time):
+                value = dateformat.format(value, formatting['time_format'])
+            row.append(smart_str(value))
+        write_row(row_nr + 1, row)
+    book.save(response)
     return response
--- adminactions/tests/__init__.py
+++ adminactions/tests/__init__.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from .mass_update import *  # NOQA
 from .exports import *  # NOQA
 from .merge import MergeTest, MergeTestApi  # NOQA
+from .export_api import * # NOQA
 
 if getattr(settings, 'ENABLE_SELENIUM', True):
     try:
