10c66e53742e85eaf28e5319aebfd3b1f111b578
sax <s.apostolico@gmail.com>
2013-12-30 12:10:44 +0100

    fixes transaction management for django<1.6

--- adminactions/compat.py
+++ adminactions/compat.py
@@ -1,7 +1,4 @@
 try:
-    from django.db.transaction import atomic
-    import django.db.transaction as t
-
-except:
-    from django.db.transaction import commit_on_success as atomic, rollback, commit_manually
-    import django.db.transaction as t
+    from django.db.transaction import atomic, rollback
+except:  # django<1.6
+    from django.db.transaction import commit_on_success as atomic, rollback
