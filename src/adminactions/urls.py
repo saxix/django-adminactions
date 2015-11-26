a1c4ace63761bcfae99c3603d3257bd76f204c8d
sax <s.apostolico@gmail.com>
2015-11-26 18:01:42 +0100

    fixes travis config (I hope)

--- src/adminactions/urls.py
+++ src/adminactions/urls.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from adminactions.views import format_date
 
