0e94dd5f2ca47e192a2ba6b43ff5ea2ad51b5c89
florianm <florian.mayer@dpaw.wa.gov.au>
2016-10-17 16:56:17 +0800

    make render() fail early and loud, and landscape code checker happier by not accepting any unused arguments)

--- src/adminactions/compat.py
+++ src/adminactions/compat.py
@@ -101,15 +101,17 @@ elif version >= (1, 4):  # noqa
 
 # Django 1.10 render() vs Django < 1.10 render_to_response()
 if version < (1, 9):
-    def render(request, template, context=None, **kwargs):
+    def render(request, template, context=None):
         """A render() wrapper around render_to_response() for Django < 1.9.
 
         There are more arguments to render() and render_to_response(),
         and they are different across django versions.
 
         As this wrapper is only ever used from within this package,
-        only request, template and context are handled, others are accepted
-        but will be discarded.
+        only request, template and context are handled.
+        Other args and kwargs are not accepted and will raise an Exception,
+        which will fail louder and earlier than accepting and silently
+        discarding them.
         """
         return render_to_response(template, RequestContext(request, context))
 else:
