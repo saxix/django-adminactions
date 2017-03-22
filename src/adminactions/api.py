f7dfb618eb2249c2d89cfad5f83a6c636d02b3d3
Serhiy Zahoriya <serhiy.int@gmail.com>
2017-03-22 16:03:33 +0200

    Use get_accessor_name() instead of field name

--- src/adminactions/api.py
+++ src/adminactions/api.py
@@ -105,7 +105,7 @@ def merge(master, other, fields=None, commit=False, m2m=None, related=None):  # 
                 if isinstance(field_object, ManyToManyField) \
                         and not field_object.rel.through._meta.auto_created:
                     continue
-                source_m2m = getattr(other, field_object.name)
+                source_m2m = getattr(other, field_object.get_accessor_name())
                 for r in source_m2m.all():
                     all_m2m[fieldname].append(r)
         if related:
