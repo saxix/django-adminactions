33fe9f8c89994a72c44c19925e9efb0d72678014
sax <s.apostolico@gmail.com>
2017-05-05 10:20:47 +0200

    trying to fix travis 1

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ def fread(fname):
     return open(rel('install.any.pip')).read() + open(rel(fname)).read()
 
 
+requirements = fread(reqs)
 tests_require = fread('testing.pip')
 dev_require = fread('develop.pip')
 
@@ -109,10 +110,10 @@ setup(
     cmdclass={'test': PyTest,
               'clean': Clean},
     include_package_data=True,
-    install_requires=fread(reqs),
+    install_requires=requirements,
     tests_require=tests_require,
     extras_require={
-        'test': tests_require,
+        'test': requirements+tests_require,
         'dev': dev_require + tests_require,
     },
     zip_safe=False,
@@ -124,10 +125,12 @@ setup(
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Intended Audience :: Developers'],
     long_description=open('README.rst').read()
 )
