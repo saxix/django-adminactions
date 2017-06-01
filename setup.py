49238c2d3966ebece36c4dfc0af520e44a5939c8
sax <s.apostolico@gmail.com>
2017-06-01 11:37:58 +0200

    removes pin in virtualenv version

--- setup.py
+++ setup.py
@@ -5,88 +5,19 @@ from __future__ import absolute_import
 import imp
 import os
 import sys
-from distutils import log
-from distutils.command.clean import clean as CleanCommand
-from distutils.dir_util import remove_tree
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test as TestCommand
 
 ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__)))
 init = os.path.join(ROOT, 'src', 'adminactions', '__init__.py')
+# app = imp.load_source('concurrency', init)
 
-if sys.version_info[0] == 2:
-    reqs = 'install.py2.pip'
-    app = imp.load_source('adminactions', init)
-elif sys.version_info[0] == 3:
-    reqs = 'install.py3.pip'
-    if sys.version_info[1] in [3,4]:
-        from importlib.machinery import SourceFileLoader
-        app = SourceFileLoader("adminactions", init).load_module()
-    elif sys.version_info[1] in [5,6]:
-        import importlib.util
-        spec = importlib.util.spec_from_file_location("adminactions", init)
-        app = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(app)
+
+reqs = "install.py{}.pip".format(sys.version_info[0])
 
 rel = lambda fname: os.path.join(os.path.dirname(__file__),
                                  'src',
                                  'requirements', fname)
-
-
-class Clean(CleanCommand):
-    user_options = CleanCommand.user_options + [
-        ('build-coverage=', 'c',
-         "build directory for coverage output (default: 'build/coverage')"),
-        ('build-tox=', 't',
-         "build directory for tox (default: '.tox')"),
-    ]
-
-    def initialize_options(self):
-        self.build_coverage = None
-        self.build_help = None
-        self.build_tox = None
-        CleanCommand.initialize_options(self)
-
-    def run(self):
-        if self.all:
-            for directory in (os.path.join(self.build_base, 'coverage'),
-                              os.path.join('dist'),
-                              os.path.join('.tox'),
-                              os.path.join(self.build_base, 'help')):
-                if os.path.exists(directory):
-                    remove_tree(directory, dry_run=self.dry_run)
-                else:
-                    log.warn("'%s' does not exist -- can't clean it",
-                             directory)
-        if self.build_coverage:
-            remove_tree(self.build_coverage, dry_run=self.dry_run)
-        if self.build_help:
-            remove_tree(self.build_help, dry_run=self.dry_run)
-        if self.build_tox:
-            remove_tree(self.build_tox, dry_run=self.dry_run)
-        CleanCommand.run(self)
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ['tests']
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
 
 def fread(fname):
     return open(rel('install.any.pip')).read() + open(rel(fname)).read()
@@ -97,8 +28,8 @@ tests_require = fread('testing.pip')
 dev_require = fread('develop.pip')
 
 setup(
-    name=app.NAME,
-    version=app.get_version(),
+    name='django-adminactions',
+    version="1.5",
     url='https://github.com/saxix/django-adminactions',
     download_url='https://github.com/saxix/django-adminactions',
     author='sax',
@@ -107,8 +38,6 @@ setup(
     license='MIT',
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    cmdclass={'test': PyTest,
-              'clean': Clean},
     include_package_data=True,
     install_requires=requirements,
     tests_require=tests_require,
