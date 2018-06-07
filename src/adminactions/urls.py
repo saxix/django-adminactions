cf16f0bc8e1e26a3bc44b1fafea06035b208fede
Giuseppe <giuseppe.demarco@unical.it>
2018-06-07 12:20:02 +0200

    Django 2.0 support

--- src/adminactions/urls.py
+++ src/adminactions/urls.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
-
-from django.conf.urls import url
+from django.urls import path
 
 from .views import format_date
 
 urlpatterns = (
-    url(r'^s/format/date/$', format_date, name='adminactions.format_date'),
+    path('s/format/date/', format_date, name='adminactions.format_date'),
 )
