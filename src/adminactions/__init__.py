43ccc68ae0393f62653ecefdae72a9f4fbb7f9f3
Stefano Apostolico <s.apostolico@gmail.com>
2025-02-07 22:18:01 +0100

    updates
    - add typing
    - add hatch version management
    - add github actions

--- src/adminactions/__init__.py
+++ src/adminactions/__init__.py
@@ -1,3 +1,3 @@
-VERSION = __version__ = "2.3.0"
-NAME = "django-adminactions"
-default_app_config = "adminactions.apps.Config"
+# VERSION = __version__ = "2.3.0"
+# NAME = "django-adminactions"
+# default_app_config = "adminactions.apps.Config"
--- src/adminactions/compat.py
+++ src/adminactions/compat.py
@@ -1,12 +1,21 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import django.db.transaction as t
+
+if TYPE_CHECKING:
+    from types import TracebackType
 
 
 class NoCommit(t.Atomic):
-    def __exit__(self, exc_type, exc_value, traceback):
-        super().__exit__(Exception, Exception(), None)
+    def __exit__(
+        self, exc_type: type[BaseException] | None, exc_value: BaseException | None, traceback: TracebackType | None
+    ) -> None:
+        super().__exit__(Exception, Exception(), traceback)
 
 
-def nocommit(using=None, savepoint=True, durable=False):
+def nocommit(using: str | None = None, savepoint: bool = True, durable: bool = False) -> NoCommit:
     return NoCommit(using, savepoint, durable)
 
 
